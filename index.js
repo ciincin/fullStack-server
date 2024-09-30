102d56018c4b8b2117b439947bfa67af5c61dc16
Cinthya <cinthyaredondo12@gmail.com>
2024-09-30 19:09:13 +0200

    I added enviromental variables of client and server

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ const app = express();
 const port = process.env.PORT; // Gets the port from .env file
 const mainRoutes = require('./routes/mainRoutes'); // imports the main routes
 
+const client = process.env.URL_CLIENT
+const server = process.env.URL_SERVER
 
 // middlewares
 app.use(express.json()); // To accept JSON from the client
 app.use(morgan('tiny')); // To log the client's request in the tiny format
 app.use(cors({
-  origin: 'http://localhost:5173', // to change the backend origin
+  origin: client, // to change the backend origin
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true, // To enable cookies to be sent
 })); // to allow request from different domains. To enable CORS
@@ -35,5 +37,5 @@ app.use((err, req, res, next) => {
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
-  console.log(`server listening on: http://localhost:${port}`); // log the server start message with the port
+  console.log(`server listening on: ${server}`); // log the server start message with the port
 });
